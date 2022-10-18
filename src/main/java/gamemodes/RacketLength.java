19927393cee253a9473f38766a65a8d1a2da0ee8
Victor <victor.morel@etu.u-paris.fr>
2022-10-18 12:10:50 +0200

    Mode de jeu RacketLength presque terminé, il reste a faire un translation des raquettres a chaque modification de la longueur

--- src/main/java/gamemodes/RacketLength.java
+++ src/main/java/gamemodes/RacketLength.java
@@ -2,53 +2,40 @@ package gamemodes;
 
 import javafx.scene.input.KeyCode;
 
-public class RacketLength implements Gamemode {
-	static float min_length = 10;
-	static float max_length = 100;
-	
+public class RacketLength implements Gamemode{
+	private double speed = 1;
+	private double speed_A = speed;
+	private double speed_B = speed;
+	private double max_length = 200;
+	private double min_length = 10;
 
-	public void reset(){}
+	public RacketLength(){}
 
-	public void on_key_pressed(KeyCode key){
-		if(key == KeyCode.R)
-			reset();
+	public RacketLength(double speed){
+		this.speed = speed;
+		this.speed_A = speed;
+		this.speed_B = speed;
 	}
 
+	public void reset(){
+		
+	}
+
+	public void on_key_pressed(KeyCode key){}
 	public void on_key_released(KeyCode key){}
-	
-	private double random(double min, double max){
-		return Math.random()*(max-min+1)+min;
-	}
 
 	public void update(model.Court court, double deltaT){
-		double add_length_A;
-		double add_length_B;
+		if(court.getPlayerA().get_height() > max_length || court.getPlayerA().get_height() < min_length)
+			speed_A *= -1;
 
-		if(court.getPlayerA().get_height() > min_length && court.getPlayerA().get_height() < max_length){
-			add_length_A = random(-10, 10);
-		}else if (court.getPlayerA().get_height() < min_length){
-			add_length_A = random(0, 10);
-		}else{
-			add_length_A = random(-10, 0);
-		}
-
-		if(court.getPlayerB().get_height() > min_length && court.getPlayerB().get_height() < max_length){
-			add_length_B = random(-10, 10);
-		}else if (court.getPlayerB().get_height() < min_length){
-			add_length_B = random(0, 10);
-		}else{
-			add_length_B = random(-10, 0);
-		}
-
-		court.getPlayerA().add_height(add_length_A);
-		court.getPlayerA().add_y(add_length_A / 2);
-		court.getPlayerB().add_height(add_length_B);
-		court.getPlayerB().add_y(add_length_B / 2);
-
-
-
+		if(court.getPlayerB().get_height() > max_length || court.getPlayerB().get_height() < min_length)
+			speed_B *= -1;
+	
+		court.getPlayerA().add_height(speed_A);
+		court.getPlayerA().add_y(speed_A / 2);
+		court.getPlayerB().add_height(speed_B);
+		court.getPlayerB().add_y(speed_B / 2);
 	}
 
 	public void render(gui.GameView view, model.Court court){}
-
 }
