aa8cc38938c5dd7321cc1ff339d19c5904ec9b8e
Amelia Coutard <eliottulio.coutard@gmail.com>
2022-11-03 10:41:03 +0100

    Implemented continuous collisions (collisions that take the velocity into account, to avoid skipping over obstacles)

--- src/main/java/game-objects/GameObject.java
+++ src/main/java/game-objects/GameObject.java
@@ -91,10 +91,66 @@ public abstract class GameObject {
 		dx += x;
 		dy += y;
 	}
-	public final boolean collides(GameObject o) {
-		return   get_up()   < o.get_down()
-		    && o.get_up()   <   get_down()
-		    &&   get_left() < o.get_right()
-		    && o.get_left() <   get_right();
+	/* Math for the collisions:
+	 * u = up, d = down, l = left, r = right, x = dx, y = dy
+	 * collision when: l1 + x1t < r2 + x2t ∧ l2 + x2t < r1 + x1t
+	 *               ∧ u1 + y1t < d2 + y2t ∧ u2 + y2t < d1 + y1t
+	 *             <=> l1 - r2 < (x2 - x1)t < r1 - l2
+	 *               ∧ u1 - d2 < (y2 - y1)t < d1 - u2
+	 *                 on nomme nos variables pour plus de clarté
+	 *             <=> a < bt < c ∧ d < et < f
+	 *
+	 */
+	public final boolean collides(GameObject o, double dt) {
+		final double epsilon = 0.0000001;
+		final double a = get_left() - o.get_right();
+		final double b = o.get_dx() - get_dx();
+		final double c = get_right() - o.get_left();
+		final double d = get_up() - o.get_down();
+		final double e = o.get_dy() - get_dy();
+		final double f = get_down() - o.get_up();
+		double min_t;
+		double max_t;
+		if (b < -epsilon) {
+			if (a / b < c / b) {
+				return false;
+			}
+			min_t = c / b;
+			max_t = a / b;
+		} else if (epsilon < b) {
+			if (a / b > c / b) {
+				return false;
+			}
+			min_t = a / b;
+			max_t = c / b;
+		} else { // b is around about 0.
+			if (c < a || 0 < a || c < 0) {
+				return false;
+			}
+			min_t = -100000;
+			max_t =  100000;
+		}
+		if (e < -epsilon) {
+			if (d / e < f / e) {
+				return false;
+			}
+			min_t = Math.max(min_t, f / e);
+			max_t = Math.min(max_t, d / e);
+		} else if (epsilon < e) {
+			if (d / e > f / e) {
+				return false;
+			}
+			min_t = Math.max(min_t, d / e);
+			max_t = Math.min(max_t, f / e);
+		} else { // e is around about 0.
+			if (f < d || 0 < d || f < 0) {
+				return false;
+			}
+		}
+		if (max_t < 0 || min_t > dt) {
+			return false;
+		}
+		final double t = Math.max(0, min_t); // Unused for now, will be used later to calculate the collision point.
+		return true;
 	}
 }
