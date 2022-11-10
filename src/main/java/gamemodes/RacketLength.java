4ded5065a32aef87faa898b75febea65a37c03f9
ilian <iliancrague@gmail.com>
2022-11-10 18:23:22 +0100

    avec la modif sur racketlength

--- src/main/java/gamemodes/RacketLength.java
+++ src/main/java/gamemodes/RacketLength.java
@@ -23,15 +23,21 @@ public class RacketLength implements Gamemode {
 	public void on_key_released(KeyCode key) {}
 
 	public boolean update(model.Court court, double dt) {
-		if (court.get_player_a().get_height() > max_length || court.get_player_a().get_height() < min_length) {
-			speed_a *= -1;
-		}
-		if (court.get_player_b().get_height() > max_length || court.get_player_b().get_height() < min_length) {
-			speed_b *= -1;
-		}
+	        if(court.get_player_a().get_height() > max_length  || court.get_player_a().get_height() < min_length){
+	    if(!((court.get_player_a().get_height() > max_length + speed_a && speed_a < 0)||(court.get_player_a().get_height() < min_length + speed_a && speed_a > 0))){
+		speed_a *= -1;
+	    }
+	    
+	}
+	if(court.get_player_b().get_height() > max_length || court.get_player_b().get_height() < min_length)
+	    if(!((court.get_player_b().get_height() > max_length + speed_b && speed_b < 0)||(court.get_player_b().get_height() < min_length + speed_b && speed_b > 0))){
+		speed_b *= -1;
+	    }
 	
-		court.get_player_a().set_height(court.get_player_a().get_height() + speed_a);
-		court.get_player_b().set_height(court.get_player_b().get_height() + speed_b);
+	court.get_player_a().set_height(court.get_player_a().get_height() +speed_a);
+	court.get_player_a().change_y(-speed_a / 2);
+	court.get_player_b().set_height(court.get_player_b().get_height() +speed_b);
+	court.get_player_b().change_y(-speed_b / 2);
 		return false;
 	}
 
