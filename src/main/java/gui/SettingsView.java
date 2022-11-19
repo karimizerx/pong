c6bb47bb2b064227f8afd99c16d341a259bbcae6
S1nj <marwan.azizi@gmail.com>
2022-11-19 09:59:46 +0100

    Interface de sélection des gamemodes dans SettingsView, via checkbox

---
+++ src/main/java/gui/SettingsView.java
@@ -0,0 +1,55 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.Court;
+import model.Settings;
+
+import game_objects.Racket;
+import gamemodes.*;
+
+public class SettingsView {
+	private Settings settings;
+
+	public SettingsView(Stage  primaryStage, Settings s) {
+		settings = s;
+
+		var root = new BorderPane();
+		var box = new VBox();
+		box.setSpacing(10);
+
+		for (Gamemode gamemode : settings.possible_gamemodes) {
+			var mode_checkbox = new CheckBox(gamemode.getName());
+
+			mode_checkbox.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent e) {
+					CheckBox selected = (CheckBox) e.getSource();
+					if (selected.isSelected()) {
+						settings.gamemodes.add(gamemode);
+					} else {
+						settings.gamemodes.remove(gamemode);
+					}
+				}
+			});
+
+			box.getChildren().add(mode_checkbox);
+		}
+
+		root.setCenter(box);
+		box.setAlignment(Pos.CENTER);
+
+		var scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
