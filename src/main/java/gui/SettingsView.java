f88b994ddc82a6cd9810213f1f38905a958a1ddf
S1nj <marwan.azizi@gmail.com>
2022-11-19 21:08:31 +0100

    sélection des modes de jeux ainsi que les les sous modes de bonus malus

--- src/main/java/gui/SettingsView.java
+++ src/main/java/gui/SettingsView.java
@@ -25,8 +25,35 @@ public class SettingsView {
 
 		var root = new BorderPane();
 		var box = new VBox();
+		var sub_box = new VBox();
 		box.setSpacing(10);
+		sub_box.setSpacing(5);
 
+		var retour_button = new Button("OK");
+		retour_button.setOnAction(retour);
+		box.getChildren().add(retour_button);
+
+		// construction de la liste des modes de bonus malus
+		for(Gamemode bm_gamemode : settings.possible_bm_gamemodes) {
+			var sub_mode_checkbox = new CheckBox(bm_gamemode.getName() + " (Bonus-Malus)");
+
+			sub_mode_checkbox.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent e) {
+					CheckBox sub_selected = (CheckBox) e.getSource();
+					if (sub_selected.isSelected()) {
+						settings.bm_gamemodes.add(bm_gamemode);
+					} else {
+						settings.bm_gamemodes.remove(bm_gamemode);
+					}
+				}
+			});
+
+			sub_box.getChildren().add(sub_mode_checkbox);
+		}
+		sub_box.setAlignment(Pos.CENTER);
+
+		// construction de la liste des modes
 		for (Gamemode gamemode : settings.possible_gamemodes) {
 			var mode_checkbox = new CheckBox(gamemode.getName());
 
@@ -36,8 +63,18 @@ public class SettingsView {
 					CheckBox selected = (CheckBox) e.getSource();
 					if (selected.isSelected()) {
 						settings.gamemodes.add(gamemode);
+
+						if (gamemode instanceof Bonus_Malus) {
+							box.getChildren().add(sub_box);
+						}
+
 					} else {
 						settings.gamemodes.remove(gamemode);
+
+						if (gamemode instanceof Bonus_Malus) {
+							box.getChildren().remove(sub_box);
+							settings.reset_bonus_malus();
+						}
 					}
 				}
 			});
@@ -45,9 +82,6 @@ public class SettingsView {
 			box.getChildren().add(mode_checkbox);
 		}
 
-		var retour_button = new Button("OK");
-		retour_button.setOnAction(retour);
-		box.getChildren().add(retour_button);
 
 		root.setCenter(box);
 		box.setAlignment(Pos.CENTER);
