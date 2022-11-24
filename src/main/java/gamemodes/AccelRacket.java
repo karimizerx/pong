e0d9fbdd666599325951371df80abb90e66f1d45
friedman <eliot.friedmann@etu.u_paris.fr>
2022-11-24 10:15:04 +0100

    ajout du mini jeu inversion de racket & accelracket

--- src/main/java/gamemodes/AccelRacket.java
+++ src/main/java/gamemodes/AccelRacket.java
@@ -7,6 +7,7 @@ import model.Court;
 public class AccelRacket implements Gamemode {
 	Court court1;
 	int mult;
+	double vitesse_nv;
 	public AccelRacket(int x) {
 		this.mult=x;
 	}
@@ -16,18 +17,21 @@ public class AccelRacket implements Gamemode {
 	public void on_key_released(KeyCode key) {}
 	public void on_ball_touched_racket(model.Court court, boolean left) {}
 	public void on_ball_left_terrain(model.Court court, boolean left) {}
+
 	boolean desactive=false;
 	public void update(model.Court court, double dt) {
 		if(!desactive){
-            court.add_racket_speed(court.get_racket_speed()*mult);
+			this.vitesse_nv=court.get_racket_speed()*mult;
+            court.add_racket_speed(vitesse_nv);
 			desactive=!desactive;
 		}
-
 		court1=court;
 	}
+
 	public void reset() {
 			if(court1==null)return;
-			court1.add_racket_speed(court1.get_racket_speed()/mult);
+			court1.add_racket_speed(-vitesse_nv);
+			desactive=false;
 		
 	}
 	public void render(gui.GameView view, model.Court court) {}
---
+++ src/main/java/gamemodes/InversionToucheGauche.java
@@ -0,0 +1,42 @@
+package gamemodes;
+
+import javafx.scene.input.KeyCode;
+import java.util.Random;
+import model.Court;
+
+public class InversionToucheGauche implements Gamemode {
+	boolean desactive=false;
+	Court court1;
+	boolean deja_reset = false;	
+	public InversionToucheGauche() {}
+
+	public void on_key_pressed(KeyCode key) {}
+	public void on_key_released(KeyCode key) {}
+	public void on_ball_touched_racket(model.Court court, boolean left) {}
+	public void on_ball_left_terrain(model.Court court, boolean left) {	}
+	
+	
+
+	public void update(model.Court court, double dt) {
+		court1 = court;
+		if(!desactive){
+			KeyCode k = court.get_player_a().get_down_key();
+			court.get_player_a().set_down_key(court.get_player_a().get_up_key());
+			court.get_player_a().set_up_key(k);		
+			desactive=true;
+		}
+	
+	}
+
+	public void reset() {		
+		if(court1 == null || deja_reset){deja_reset = false;return;}
+		KeyCode k = court1.get_player_a().get_down_key();
+		court1.get_player_a().set_down_key(court1.get_player_a().get_up_key());
+		court1.get_player_a().set_up_key(k);
+		deja_reset = true;
+		desactive = false;
+
+		//desactive=!desactive;
+	}
+	public void render(gui.GameView view, model.Court court) {}
+}
