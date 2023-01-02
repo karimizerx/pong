f3925ceb387c208b41b3c077f6adbf0f01471809
Amelia Coutard <eliottulio.coutard@gmail.com>
2023-01-02 13:03:53 +0100

    Refactored the AI: no functionality changed

--- src/main/java/gamemodes/Ia.java
+++ src/main/java/gamemodes/Ia.java
@@ -1,18 +1,12 @@
 package gamemodes;
 
-import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
-import javafx.scene.layout.Pane;
-
 import javafx.scene.input.KeyCode;
-import game_objects.Racket;
-import game_objects.Ball;
+
 import model.Court;
 
 public class Ia implements Gamemode {
 	private double difficulty;
 	private boolean gauche;
-	public Court court1;
 
 	public Ia(double diff, boolean g) {
 		difficulty = diff;
@@ -29,66 +23,20 @@ public class Ia implements Gamemode {
 	public void on_ball_left_terrain(model.Court court, boolean left) {}
 
 	public void update(model.Court court, double dt) {
-		if (gauche) {
-			if (difficulty == 1) {
-				if (court.get_ball().get_middle_y() > court.get_player_a().get_middle_y()) {
-					court.get_player_a().set_direction(+0.5);
-				} else {
-					court.get_player_a().set_direction(-0.5);
-				}
+		var player = gauche ? court.get_player_a() : court.get_player_b();
+		var ball = court.get_ball();
+		if (difficulty <= 3) {
+			if (ball.get_middle_y() > player.get_middle_y()) {
+				player.set_direction(+0.25 + difficulty * 0.25);
+			} else {
+				player.set_direction(-0.25 - difficulty * 0.25);
 			}
-			if (difficulty == 2) {
-				if (court.get_ball().get_middle_y() > court.get_player_a().get_middle_y()) {
-					court.get_player_a().set_direction(+0.75);
-				} else {
-					court.get_player_a().set_direction(-0.75);
-				}
-			}
-			if (difficulty == 3) {
-				if (court.get_ball().get_middle_y() > court.get_player_a().get_middle_y()) {
-					court.get_player_a().set_direction(+1.0);
-				} else {
-					court.get_player_a().set_direction(-1.0);
-				}
-			}
-			if (difficulty == 4) {
-				court.get_player_a().set_direction((court.get_ball().get_middle_y() - court.get_player_a().get_middle_y()) / court.get_racket_speed() / 0.01);
-			}
-		} else {
-			if (difficulty == 1) {
-				if (court.get_ball().get_middle_y() > court.get_player_b().get_middle_y()) {
-					court.get_player_b().set_direction(+0.5);
-				} else {
-					court.get_player_b().set_direction(-0.5);
-				}
-			}
-			if (difficulty == 2) {
-				if (court.get_ball().get_middle_y() > court.get_player_b().get_middle_y()) {
-					court.get_player_b().set_direction(+0.75);
-				} else {
-					court.get_player_b().set_direction(-0.75);
-				}
-			}
-			if (difficulty == 3) {
-				if (court.get_ball().get_middle_y() > court.get_player_b().get_middle_y()) {
-					court.get_player_b().set_direction(+1.0);
-				} else {
-					court.get_player_b().set_direction(-1.0);
-				}
-			}
-			if (difficulty == 4) {
-				court.get_player_b().set_direction((court.get_ball().get_middle_y() - court.get_player_b().get_middle_y()) / court.get_racket_speed() / 0.01);
-			}
-		}
-		this.court1 = court;
-	}
-
-
-	public void reset() {
-		if (this.court1 != null) {
-			court1.get_player_a().set_y(this.court1.get_height() / 2);
+		} else if (difficulty == 4) {
+			player.set_direction((ball.get_middle_y() - player.get_middle_y()) / court.get_racket_speed() / 0.01);
 		}
 	}
+
+	public void reset() {}
 
 	public void render() {}
 	public void no_render() {}
